65cd590a63f4cdbeed5cf6bb819cd4150b40c17a
Nate Wright <natew@notthisway.com>
2023-06-20 11:34:50 +0100

    Show loading and incompatible messages based on browser features

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,13 @@ import './assets/css/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/**
+ * Don't load the app if the browser does not support
+ * IntersectionObserver or ResizeObserver
+ */
+if (('IntersectionObserver' in window) &&
+  ('IntersectionObserverEntry' in window) &&
+  ('intersectionRatio' in window.IntersectionObserverEntry.prototype) &&
+  ('ResizeObserver' in window !== false)) {
+    createApp(App).mount('#app')
+}
